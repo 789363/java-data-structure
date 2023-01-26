f3e224f8beb941fc382f4ad33e522097f51ab2e5
YuWei <40884505+789363@users.noreply.github.com>
2023-01-27 00:20:09 +0800

    BinarySearchTree
    BinarySearchTree

---
+++ bts.java
@@ -0,0 +1,122 @@
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+class BinarySearchTree {
+    Node root;
+
+    BinarySearchTree() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insert(root, key);
+    }
+
+    Node insert(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key)
+            root.left = insert(root.left, key);
+        else if (key > root.key)
+            root.right = insert(root.right, key);
+
+        return root;
+    }
+
+    void preorder() {
+        preorder(root);
+    }
+
+    void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    void search(int key) {
+        Node foundNode = search(root, key);
+        if (foundNode != null) {
+            System.out.println("Node with key " + key + " found.");
+        } else {
+            System.out.println("Node with key " + key + " not found.");
+        }
+    }
+
+    Node search(Node root, int key) {
+        if (root == null || root.key == key) {
+            return root;
+        }
+        if (root.key > key) {
+            return search(root.left, key);
+        }
+        return search(root.right, key);
+    }
+
+    void delete(int key) {
+        root = delete(root, key);
+    }
+
+    Node delete(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+        if (key < root.key) {
+            root.left = delete(root.left, key);
+        } else if (key > root.key) {
+            root.right = delete(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            root.key = minValue(root.right);
+            root.right = delete(root.right, root.key);
+        }
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    Node minValueNode(Node root) {
+        Node current = root;
+
+        while (current.left != null) {
+            current = current.left;
+        }
+        System.out.println("min=" + current.key);
+        return current;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.minValue();
+        tree.delete(70);
+        tree.preorder();
+
+    }
+}

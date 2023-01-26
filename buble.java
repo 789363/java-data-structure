93c30eea9c46583ae983c0186262236808058bbe
YuWei <40884505+789363@users.noreply.github.com>
2023-01-27 01:52:56 +0800

    Bubble Sort
    Bubble Sort

---
+++ buble.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class bubble {
+    // 泡沫排序
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = { 1, 2, 9, 6, 3, 7, 8, 5, 4 };
+        bubbleSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+    }
+}
